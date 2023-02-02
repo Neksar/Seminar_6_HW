85cd61a6a85d7ce92add99fdda22838b217b3dac
Maksim <m.bondarenko1988@yandex.ru>
2023-02-02 23:36:27 +0300

    Done second homework to six seminar about C sharp

---
+++ homework_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("Введите число b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите число k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите число b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите число k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if(k1 != k2)
+{
+    double x = (b1 - b2) / (-k1 + k2);
+    double y = k2 * x + b2;
+    System.Console.WriteLine($"Точка пересечения прямых равна: ({x}, {y})");
+}
+else
+{
+    System.Console.WriteLine("Точек пересечения нет, прямые параллельны!");
+}
